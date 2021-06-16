dc74288b1e51c92a944b31ff1f38160d2e644afc
ESRICCI <ESRICCI@DESKTOP-SUS05U6>
2021-06-16 20:44:23 -0300

    Projeto Ricci

---
+++ src/br/com/projeto/jdbc/ConnectionFactory.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projeto.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Caio Duarte
+ */
+public class ConnectionFactory {
+
+    public Connection getConnection() {
+
+        try {
+
+            //return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/bdvendas", "usuariocurso", "123");
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/bdvendas", "root", "EfCr2608");
+
+        } catch (Exception erro) {
+            throw new RuntimeException(erro);
+        }
+
+    }
+
+    
+     
+
+  
+}
